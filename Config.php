b46ae999981a9cbb1c9e6e89de10135421e40fd1
alvarito4 <130836969+alvarito4@users.noreply.github.com>
2026-01-16 07:45:54 +0100

    V1 config

---
+++ Config.php
@@ -0,0 +1,7 @@
+<?php
+// config.php
+// Sustituye estos hashes por los tuyos propios
+const ADMIN_USER_HASH = 'PON_AQUI_HASH_USUARIO';
+const ADMIN_PASS_HASH = 'PON_AQUI_HASH_PASSWORD';
+
+session_start();
